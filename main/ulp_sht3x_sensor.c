eaec13d557c0f30c3b0ee517d0ea69b79b4b0b9e
KIMATA Tetsuya <kimata@green-rabbit.net>
2018-06-12 23:49:42 +0900

    fix code.

--- main/ulp_sht3x_sensor.c
+++ main/ulp_sht3x_sensor.c
@@ -15,13 +15,16 @@
 #include "driver/rtc_io.h"
 #include "lwip/sockets.h"
 
+#include "esp32/ulp.h"
+
 #define LOG_LOCAL_LEVEL ESP_LOG_INFO
 #include "esp_log.h"
-#include "esp_wifi.h"
-#include "esp_event_loop.h"
-#include "esp_task_wdt.h"
+
 #include "esp_adc_cal.h"
-#include "esp32/ulp.h"
+#include "esp_event_loop.h"
+#include "esp_spi_flash.h"
+#include "esp_task_wdt.h"
+#include "esp_wifi.h"
 
 #include <stdlib.h>
 #include <stdbool.h>
@@ -30,6 +33,11 @@
 #include "cJSON.h"
 
 #include "ulp_main.h"
+
+#include "wifi_config.h"
+// wifi_config.h should define followings.
+// #define WIFI_SSID "XXXXXXXX"            // WiFi SSID
+// #define WIFI_PASS "XXXXXXXX"            // WiFi Password
 
 ////////////////////////////////////////////////////////////
 // Configuration
@@ -40,9 +48,10 @@
 #define WIFI_HOSTNAME   "ESP32-outdoor2"  // module's hostname
 #define SENSE_INTERVAL  30              // sensing interval
 #define SENSE_COUNT     20              // buffering count
+#define SENSE_COUNT_MAX 30              // buffering count
 
-#define WIFI_SSID "XXXXXXXX"            // WiFi SSID
-#define WIFI_PASS "XXXXXXXX"            // WiFi Password
+/* #define WIFI_SSID "XXXXXXXX"            // WiFi SSID */
+/* #define WIFI_PASS "XXXXXXXX"            // WiFi Password */
 
 #define ADC_VREF        1128            // ADC calibration data
 
@@ -82,6 +91,31 @@ extern const uint8_t ulp_main_bin_end[]   asm("_binary_ulp_main_bin_end");
     "Content-Length: %d\r\n" \
     "\r\n" \
     "json=%s"
+
+
+//////////////////////////////////////////////////////////////////////
+// Error Handling
+static void _error_check_failed(esp_err_t rc, const char *file, int line,
+                                const char *function, const char *expression)
+{
+    ets_printf("ESP_ERROR_CHECK failed: esp_err_t 0x%x", rc);
+#ifdef CONFIG_ESP_ERR_TO_NAME_LOOKUP
+    ets_printf(" (%s)", esp_err_to_name(rc));
+#endif //CONFIG_ESP_ERR_TO_NAME_LOOKUP
+    ets_printf(" at 0x%08x\n", (intptr_t)__builtin_return_address(0) - 3);
+    if (spi_flash_cache_enabled()) { // strings may be in flash cache
+        ets_printf("file: \"%s\" line %d\nfunc: %s\nexpression: %s\n", file, line, function, expression);
+    }
+}
+
+#define ERROR_RETURN(x, fail) do {                                      \
+        esp_err_t __err_rc = (x);                                       \
+        if (__err_rc != ESP_OK) {                                       \
+            _error_check_failed(__err_rc, __FILE__, __LINE__,           \
+                                __ASSERT_FUNC, #x);                     \
+            return fail;                                                \
+        }                                                               \
+    } while(0);
 
 //////////////////////////////////////////////////////////////////////
 // Sensor Function
@@ -144,11 +178,12 @@ uint32_t get_battery_voltage(void)
 
     adc1_config_width(ADC_WIDTH_BIT_12);
     adc1_config_channel_atten(BATTERY_ADC_CH, ADC_ATTEN_11db);
-    esp_adc_cal_get_characteristics(ADC_VREF, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12,
-                                    &characteristics);
+    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, ADC_VREF,
+                             &characteristics);
 
     for (uint32_t i = 0; i < BATTERY_ADC_SAMPLE; i++) {
-        ad_volt_list[i] = adc1_to_voltage(BATTERY_ADC_CH, &characteristics);
+        ESP_ERROR_CHECK(esp_adc_cal_get_voltage(BATTERY_ADC_CH,
+                                                &characteristics, ad_volt_list + i));
     }
 
     qsort(ad_volt_list, BATTERY_ADC_SAMPLE, sizeof(uint32_t),
@@ -182,25 +217,28 @@ static cJSON *sense_json(uint32_t battery_volt, wifi_ap_record_t *ap_record,
                          uint32_t wifi_con_msec)
 {
     sense_data_t *sense_data = (sense_data_t *)&ulp_sense_data;
-
     cJSON *root = cJSON_CreateArray();
 
-    for (uint32_t i = 0; i < SENSE_COUNT; i++) {
+    for (uint32_t i = 0; i < ulp_sense_count; i++) {
+        cJSON *item = cJSON_CreateObject();
+        uint32_t index = ulp_sense_count - i - 1;
+
         if (!sense_data_check_crc(sense_data + i)) {
             ESP_LOGE(TAG, "CRC ERROR OCCURED (%d)", i);
             continue;
         }
-        cJSON *item = cJSON_CreateObject();
+
         cJSON_AddNumberToObject(item, "temp", sense_calc_temp(sense_data + i));
         cJSON_AddNumberToObject(item, "humi", sense_calc_humi(sense_data + i));
         cJSON_AddStringToObject(item, "hostname", WIFI_HOSTNAME);
-        cJSON_AddNumberToObject(item, "self_time", SENSE_INTERVAL * i); // negative offset
+        cJSON_AddNumberToObject(item, "self_time", SENSE_INTERVAL * index); // negative offset
 
-        if (i == 0) {
+        if (index == 0) {
             cJSON_AddNumberToObject(item, "battery", battery_volt);
             cJSON_AddNumberToObject(item, "wifi_ch", ap_record->primary);
             cJSON_AddNumberToObject(item, "wifi_rssi", ap_record->rssi);
             cJSON_AddNumberToObject(item, "wifi_con_msec", wifi_con_msec);
+            cJSON_AddNumberToObject(item, "retry", ulp_sense_count - SENSE_COUNT);
         }
 
         cJSON_AddItemToArray(root, item);
@@ -209,16 +247,17 @@ static cJSON *sense_json(uint32_t battery_volt, wifi_ap_record_t *ap_record,
     return root;
 }
 
-static void process_sense_data(uint32_t connect_msec, uint32_t battery_volt)
+static bool process_sense_data(uint32_t connect_msec, uint32_t battery_volt)
 {
     wifi_ap_record_t ap_record;
     char buffer[sizeof(EXPECTED_RESPONSE)];
+    bool result = false;
 
-    ESP_ERROR_CHECK(esp_wifi_sta_get_ap_info(&ap_record));
+    ERROR_RETURN(esp_wifi_sta_get_ap_info(&ap_record), false);
 
     int sock = connect_server();
     if (sock == -1) {
-        return;
+        return false;
     }
 
     cJSON *json = sense_json(battery_volt, &ap_record, connect_msec);
@@ -238,10 +277,14 @@ static void process_sense_data(uint32_t connect_msec, uint32_t battery_volt)
             break;
         }
         ESP_LOGI(TAG, "FLUENTD POST SUCCESSFUL");
+
+        result = true;
     } while (0);
 
     close(sock);
     cJSON_Delete(json);
+
+    return result;
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -251,8 +294,8 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
     switch(event->event_id) {
     case SYSTEM_EVENT_STA_START:
         ESP_LOGI(TAG, "SYSTEM_EVENT_STA_START");
-        ESP_ERROR_CHECK(tcpip_adapter_set_hostname(TCPIP_ADAPTER_IF_STA, WIFI_HOSTNAME));
-        ESP_ERROR_CHECK(esp_wifi_connect());
+        ERROR_RETURN(tcpip_adapter_set_hostname(TCPIP_ADAPTER_IF_STA, WIFI_HOSTNAME), ESP_FAIL);
+        ERROR_RETURN(esp_wifi_connect(), ESP_FAIL);
         break;
     case SYSTEM_EVENT_STA_GOT_IP:
         ESP_LOGI(TAG, "SYSTEM_EVENT_STA_GOT_IP");
@@ -268,25 +311,25 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
     return ESP_OK;
 }
 
-static void init_wifi()
+static bool wifi_init()
 {
     esp_err_t ret;
 
     ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
+        ERROR_RETURN(nvs_flash_erase(), false);
         ret = nvs_flash_init();
     }
-    ESP_ERROR_CHECK(ret);
+    ERROR_RETURN(ret, false);
 
     tcpip_adapter_init();
 
-    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
+    ERROR_RETURN(esp_event_loop_init(event_handler, NULL), false);
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
 
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ERROR_RETURN(esp_wifi_init(&cfg), false);
+    ERROR_RETURN(esp_wifi_set_mode(WIFI_MODE_STA), false);
 
 #ifdef WIFI_SSID
     wifi_config_t wifi_config = {
@@ -296,32 +339,36 @@ static void init_wifi()
         },
     };
     wifi_config_t wifi_config_cur;
-    ESP_ERROR_CHECK(esp_wifi_get_config(WIFI_IF_STA, &wifi_config_cur));
+    ERROR_RETURN(esp_wifi_get_config(WIFI_IF_STA, &wifi_config_cur), false);
 
     if (strcmp((const char *)wifi_config_cur.sta.ssid, (const char *)wifi_config.sta.ssid) ||
         strcmp((const char *)wifi_config_cur.sta.password, (const char *)wifi_config.sta.password)) {
         ESP_LOGI(TAG, "SAVE WIFI CONFIG");
-        ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+        ERROR_RETURN(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config), false);
     }
 #endif
+
+    return true;
 }
 
-static esp_err_t wifi_connect()
+static bool wifi_connect()
 {
     xSemaphoreTake(wifi_conn_done, portMAX_DELAY);
-    ESP_ERROR_CHECK(esp_wifi_start());
+    ERROR_RETURN(esp_wifi_start(), false);
     if (xSemaphoreTake(wifi_conn_done, 10000 / portTICK_RATE_MS) == pdTRUE) {
-        return ESP_OK;
+        return true;
     } else {
-        ESP_LOGE(TAG, "WIFI CONNECT TIMECOUT")
-        return ESP_FAIL;
+        ESP_LOGE(TAG, "WIFI CONNECT TIMECOUT");
+        return false;
     }
 }
 
-static void wifi_disconnect()
+static bool wifi_stop()
 {
-    ESP_ERROR_CHECK(esp_wifi_disconnect());
-    ESP_ERROR_CHECK(esp_wifi_stop());
+    ERROR_RETURN(esp_wifi_disconnect(), false);
+    ERROR_RETURN(esp_wifi_stop(), false);
+
+    return true;
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -369,21 +416,42 @@ void app_main()
     esp_log_level_set("wifi", ESP_LOG_ERROR);
 
     if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_ULP) {
+        bool status = false;
+        ulp_sense_count = ulp_sense_count & 0xFFFF; // mask
+
+        ESP_LOGI(TAG, "Send to fluentd count = %d", ulp_sense_count);
         time_start = xTaskGetTickCount();
-        init_wifi();
-        if (wifi_connect() == ESP_OK) {
+
+        if (wifi_init() && wifi_connect()) {
             connect_msec = (xTaskGetTickCount() - time_start) * portTICK_PERIOD_MS;
-            process_sense_data(connect_msec, battery_volt);
+            status = process_sense_data(connect_msec, battery_volt);
         }
-        wifi_disconnect();
+        wifi_stop();
+
+        if (status) {
+            ESP_LOGI(TAG, "OK");
+            ulp_sense_count = 0;
+            ulp_sense_full = SENSE_COUNT;
+        } else if (ulp_sense_count >= SENSE_COUNT_MAX) {
+            // count has reached max
+            ESP_LOGI(TAG, "NG");
+            ulp_sense_count = 0;
+            ulp_sense_full = SENSE_COUNT;
+        } else {
+            ESP_LOGI(TAG, "RETRY");
+            ulp_sense_full++;
+        }
     } else {
         init_ulp_program();
+        ulp_sense_count = 0;
+        ulp_sense_full = SENSE_COUNT;
     }
+
+    vTaskDelay(100);
 
     set_sleep_period();
 
     // ULP program parameter
-    ulp_sense_count = SENSE_COUNT;
     if (battery_volt > BATTERY_THRESHOLD) {
         ESP_LOGI(TAG, "Enable TPS61291 bypass mode");
         ulp_bypass_mode_enable = 1;
